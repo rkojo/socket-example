57a4c8524e863a14a097c6b3902eeecd66b3a5c0
Riku <rikukojo1@gmail.com>
2022-03-27 12:42:14 +1100

    Job scheduling possible once, recieves error on second job (need fixing)

--- client.java
+++ client.java
@@ -24,33 +24,33 @@ public class client {
       recieve = br.readLine();
       System.out.println(recieve);
 
-      dos.write("REDY\n".getBytes());
-      dos.flush();
       while (!recieve.equals("QUIT")) {
+        dos.write("REDY\n".getBytes());
+        dos.flush();
         // get job values
         String s;
         recieve = br.readLine();
         System.out.println(recieve);
         //sends getscapable
-        String job = jobid(recieve);
-        s = "GETS Capable " + jobvalue(recieve);
+        String job = jobid(recieve); // gets id of job in jobn
+        s = "GETS Capable " + jobvalue(recieve) +"\n";
         dos.write(s.getBytes());
         dos.flush();
+        //recieves data 3 124
         recieve = br.readLine();
-        int serverno = howMany(recieve);
-        s = "OK\n";
-        dos.write(s.getBytes());
-        dos.flush();
-        //only reads a single line - need to loop and store server info into arraylist here. 
-        recieve = br.readLine();
-        System.out.println("all " + recieve);
-        String ret = getsCapable(recieve, serverno);
+        int howm = howMany(recieve);
+        String bestserver = getsCapable(dos, br, howm);
         //once it keeps and finds the best one, it should schd.
-        s = "SCHD " + job + ret + "\n";
+        // s = "OK\n";
+        // dos.write(s.getBytes());
+        dos.flush();
+        recieve = br.readLine();
+        s = "SCHD " + job + " " + bestserver + "\n";
+        dos.write(s.getBytes());
+        dos.flush();
         //once i get server info, 
         //loop until i get ".", then schedule to server
       }
-
       dos.write("QUIT\n".getBytes());
       dos.flush();
 
@@ -84,41 +84,53 @@ public class client {
     a = s.split(" ");
     return Integer.parseInt(a[2]);
   }
-  public static String getsCapable(String s, int servercount) {
+  public static String getsCapable(DataOutputStream dos, BufferedReader br, int servercount) throws IOException {
     String[] a;
     Server j = new Server();
     ArrayList<Server> aj = new ArrayList<Server>();
-    int count = 0;
-    while (count < servercount) {
+    dos.write("OK\n".getBytes());
+    dos.flush();
+    String recieve = new String();
+    boolean barrier = true;
+    while(barrier) {
+      recieve = br.readLine();
+     if (!recieve.equals(".")) {
+      System.out.println(recieve);
           //joon 0 inactive -1 4 16000 64000 0 0
-      a = s.split(" ");
-      a[count] = j.type;
+      a = recieve.split(" ");
+      System.out.println(a[0] + a[1]);
+      j.type = a[0];
       System.out.println("name = " + j.type);
       // get id
-      String l = a[count+ 1];
-      System.out.println("limit = " + l);
+      String l = a[1];
+      System.out.println("id = " + l);
       Integer la = Integer.parseInt(l);
       j.id = la;
       // get curstarttime
-      l = a[count + 3];
+      l = a[3];
       System.out.println("bootuptime = " + l);
       j.curstarttime= Integer.parseInt(l);
       // get cores
-      l = a[count + 4];
+      l = a[4];
       System.out.println("cores = " + l);
       j.cores = Integer.parseInt(l);
       // get memory
-      l = a[count + 5];
+      l = a[5];
       System.out.println("memory = " + l);
       j.memory = Integer.parseInt(l);
       // get disk
-      l = a[count + 6];
+      l = a[6];
       System.out.println("disk = " + l);
       j.disk = Integer.parseInt(l);
       aj.add(j);
-        count++;
+    } else {
+      System.out.print(recieve);
+      barrier = false;
     }
-    //need to make into separate method after readlingall the lines.
+    dos.write("OK\n".getBytes());
+    dos.flush();
+  }
+    //need to make into separate method after readlingall the lines. 
     Server first = aj.get(0);
     for(int i = 1; i <aj.size(); i++) {
       if(aj.get(i).cores > first.cores) {
@@ -127,7 +139,7 @@ public class client {
     }
     // return capable server string
 
-    return first.type + String.valueOf(first.id);
+    return first.type + " "+ String.valueOf(first.id);
 
   }
 }
